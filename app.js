098aa9fa9c29dbb5b444cfb9eec1be809c4de8e4
Yoonjieut <qmdldos@gmail.com>
2023-10-10 16:03:42 +0900

    offset을 활용한 마우스 좌표를 따오는 로직 구성

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ const modal = document.getElementById('modal');
 console.log(body, root, modal);
 console.dir(root);
 
+// ! 마우스를 올릴 때, 좌표를 따라오는 이벤트
+// * 마우스가 움직일 때마다 작동하는 mousemove와
+// * dom요소가 기준이 되는 offset을 기준으로 작성해본다.
+
+root.addEventListener('mousemove',function(event){
+  // console.dir(event);
+  console.log(event.offsetX, event.offsetY)
+  setTimeout(function(){
+    modal.style.left = event.offsetX+"px";
+    modal.style.top = event.offsetY+"px";
+  },100)
+});
