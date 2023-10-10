1f57517ec1d70358bf8e66095f03c89eef9aba5e
Yoonjieut <qmdldos@gmail.com>
2023-10-10 15:54:24 +0900

    app.js 연동 및 테스트, index.html 테스트 문구 추가

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 console.log('hi');
+
+const body = document.body;
+const root = document.getElementById('root');
+const modal = document.getElementById('modal');
+console.log(body, root, modal);
+console.dir(root);
+
