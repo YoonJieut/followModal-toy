8970b37185aa286396f544d3b86dacf1551c7034
Yoonjieut <qmdldos@gmail.com>
2023-10-10 16:39:17 +0900

    5초후 로직을 종료하는 장치 구상

--- app.js
+++ app.js
@@ -10,11 +10,37 @@ console.dir(root);
 // * 마우스가 움직일 때마다 작동하는 mousemove와
 // * dom요소가 기준이 되는 offset을 기준으로 작성해본다.
 
-root.addEventListener('mousemove',function(event){
-  // console.dir(event);
-  console.log(event.offsetX, event.offsetY)
-  setTimeout(function(){
-    modal.style.left = event.offsetX+"px";
-    modal.style.top = event.offsetY+"px";
-  },100)
+// 모달이 따라오는 로직
+function mouseOver(){
+  root.addEventListener('mousemove',function Game(event){
+    // console.dir(event);
+    // console.log(event.offsetX, event.offsetY)
+    setTimeout(function(){
+      modal.style.left = event.offsetX+"px";
+      modal.style.top = event.offsetY+"px";
+    },250)
+  });
+}
+// 모달 따라오는 로직 종료
+function mouseOverEnd(){
+  root.removeEventListener('mousemove', Game(e));
+};
+
+
+
+body.addEventListener('click', function(e){
+  console.dir(e.target);
+  // 참일 때, root를 누르면 작동하라
+  if(e.target.id === "root") {
+    console.log("true");
+    mouseOver();
+    // 5초 동안 작동, 5초후 종료
+    setTimeout(function(){
+      console.log ("종료");
+    }, 5000)
+  } else {}
 });
+
+// ! 5초간 게임을 시작한다.
+
+// 
